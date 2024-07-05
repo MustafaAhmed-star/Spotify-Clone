10ad8de3a050260ca8550f0853768f452c3097d5
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2024-07-05 06:32:33 +0300

    add templates & static files and confgs

--- music/views.py
+++ music/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1 style = "color:blue;text-align:center;backgroud-color: black">Hello Fboy</h1 ><br><h2 style = "color:blue;text-align:center">This world will f you</h2>')
+    return render(request,'index.html',{})
